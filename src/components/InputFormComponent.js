6588092d4e582843cde450fc5a1ad0caeeae8a75
Rodrick Coleman <rscole87@gmail.com>
2021-09-01 11:46:46 -0400

    add grid cols and padding to input form

--- src/components/InputFormComponent.js
+++ src/components/InputFormComponent.js
@@ -25,7 +25,7 @@ const InputForm = (props) => {
   };
 
   const handleSubmit = (job) => {
-    setApplied(status !== "saved" ? true : false)
+    setApplied(status !== "saved" ? true : false);
     props.setJobListData((prev) => prev.concat(job));
     setDate("");
     setEmployer("");
@@ -39,34 +39,48 @@ const InputForm = (props) => {
 
   return (
     <>
-      <form action="" className="px-8">
-        <input type="text" value={position} placeholder="Position" onChange={(e) => setPosition(e.target.value)} />
-        <input type="text" value={employer} placeholder="Employer" onChange={(e) => setEmployer(e.target.value)} />
-        <input type="text" value={jobSource} placeholder="Source" onChange={(e) => setJobSource(e.target.value)} />
-        <input type="text" value={url} placeholder="URL" onChange={(e) => setUrl(e.target.value)} />
+      <form action="" className="px-8 grid grid-cols-5">
+        <div>
+          <input type="text" value={position} placeholder="Position" className="p-2" onChange={(e) => setPosition(e.target.value)} />
+          <input type="text" value={employer} placeholder="Employer" className="p-2" onChange={(e) => setEmployer(e.target.value)} />
+        </div>
 
-        <select name="status" id="status" value={status ? status : setStatus("saved")} onChange={(e) => setStatus(e.target.value)}>
-          <option value="saved">Saved</option>
-          <option value="applied">Applied</option>
-          <option value="first-interview">1st Interview</option>
-          <option value="second-interview">2nd Interview</option>
-          <option value="third-plus-interview">3rd+ Interview</option>
-        </select>
+        <div>
+          <input type="text" value={jobSource} placeholder="Source" className="p-2" onChange={(e) => setJobSource(e.target.value)} />
+          <input type="text" value={url} placeholder="URL" className="p-2" onChange={(e) => setUrl(e.target.value)} />
+        </div>
 
-        <select name="response" id="response" value={employerResponse ? employerResponse : "None"} onChange={(e) => setEmployerResponse(e.target.value)}>
-          <option value="pending">Awaiting Response</option>
-          <option value="reject">Rejection</option>
-          <option value="offer">Offer</option>
-        </select>
+        <div>
+          <select name="status" id="status" value={status ? status : setStatus("saved")} className="p-2" onChange={(e) => setStatus(e.target.value)}>
+            <option value="saved">Saved</option>
+            <option value="applied">Applied</option>
+            <option value="first-interview">1st Interview</option>
+            <option value="second-interview">2nd Interview</option>
+            <option value="third-plus-interview">3rd+ Interview</option>
+          </select>
 
-        <textarea type="text" placeholder="Notes" value={notes} onChange={(e) => setNotes(e.target.value)} />
-        <input
-          type="submit"
-          onClick={(e) => {
-            e.preventDefault();
-            handleSubmit(newJob);
-          }}
-        />
+          <select name="response" id="response" value={employerResponse ? employerResponse : "None"} className="p-2" onChange={(e) => setEmployerResponse(e.target.value)}>
+            <option value="pending">Awaiting Response</option>
+            <option value="reject">Rejection</option>
+            <option value="offer">Offer</option>
+          </select>
+        </div>
+
+        <div>
+          <textarea type="text" placeholder="Notes" className="p-2" rows="1" value={notes} onChange={(e) => setNotes(e.target.value)} />
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              handleSubmit(newJob);
+            }}
+          >
+            Add Job{" "}
+          </button>
+        </div>
       </form>
     </>
   );
