da246f2cbb64a4fe71dce5c4c5f869292709fb62
Rodrick Coleman <rscole87@gmail.com>
2021-09-03 12:18:49 -0400

    resize header for mobile

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -5,7 +5,7 @@ const Header = (props) => {
   return (
     <div className="px-8 grid grid-cols-1 md:grid-cols-2 flex items-center justify-between">
       <div className="flex-1">
-        <h1 className="text-gray-500 text-7xl font-extrabold">JobR</h1>
+        <h1 className="text-gray-500 text-5xl md:text-7xl font-extrabold md:my-10">JobR</h1>
       </div>
       <div className="flex-1 flex justify-around mb-8 md:mb-0">
         <Counters jobsCount={props.jobsCount} appliedCount={props.appliedCount} rejectCount={props.rejectCount} offerCount={props.offerCount} />
