2b6eee52a9993556cfe9756b7d90c16a513ba95c
Rodrick Coleman <rscole87@gmail.com>
2021-09-04 13:59:22 -0400

    update application title and subheading in header component

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -4,8 +4,9 @@ import Counters from "./CounterComponent";
 const Header = (props) => {
   return (
     <div className="px-8 grid grid-cols-1 md:grid-cols-2 flex items-center justify-between">
-      <div className="flex-1">
-        <h1 className="text-gray-500 text-5xl md:text-7xl font-extrabold md:my-10">JobR</h1>
+      <div className="flex-1 mb-10">
+        <h1 className="text-gray-500 text-6xl mb-0 md:text-7xl font-extrabold md:mt-10 ">J.O.B.S.</h1>
+        <p className="text-xs font-bold text-gray-500 uppercase tracking-wider md:text-base">Just Offer the Best Service</p>
       </div>
       <div className="flex-1 flex justify-around mb-8 md:mb-0">
         <Counters jobsCount={props.jobsCount} appliedCount={props.appliedCount} rejectCount={props.rejectCount} offerCount={props.offerCount} />
